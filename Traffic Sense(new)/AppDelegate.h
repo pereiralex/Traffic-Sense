9310d6b9c766c345442bac232fb0d1d7cc9f8292
Alex Pereira <alex.pereira28@gmail.com>
2012-03-11 20:37:00 -0700

    Initial Commit

---
+++ Traffic Sense(new)/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  Traffic Sense(new)
+//
+//  Created by Alex Pereira on 3/11/12.
+//  Copyright (c) 2012 UBC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
